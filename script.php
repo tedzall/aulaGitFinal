dc73ad810a4c7b2f1e860524fa69eac1198d7364
Car E Bonfante <dzbola@bol.com.br>
2014-08-22 16:01:28 -0300

    Aumentando codigo

--- script.php
+++ script.php
@@ -1,7 +1,22 @@
 <?php 
 	
-	var $gitAula = 'Toda semana';
-	var $gitHubAula = 'tem aula';
+	$gitAula = 'Toda semana';
+	$gitHubAula = 'tem aula';
 		
-	echo $gitAula . ' '. $gitHubAula;	
+	echo $gitAula . ' '. $gitHubAula;
+
+	echo 'Isso é ';
+	
+	$data = getdate(); 
+	
+	switch($data['wday']){
+		case 0,1,2,3,5:
+			$situacao = 'ótimo :)';
+			break;
+		case 6,7:
+			$situacao = 'brincadeira :(';
+			break;
+	}	
+	
+	echo $situacao;
 ?>
